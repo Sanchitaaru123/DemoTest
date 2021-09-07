8ad8dac93770f08c73d7beb79f297de2bcce3c00
Sanchitaaru123 <aru.sanchita456@gmail.com>
2021-09-07 17:42:57 +0530

    Added third

---
+++ src/test/java/com/qa/test/NewTestThree.java
@@ -0,0 +1,28 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class NewTestThree {
+	 @Test
+	  public void sum() {
+		  
+		  System.out.println("SUM3");
+	  }
+	  @Test
+	  public void sub() {
+		  
+		  System.out.println("SUB3");
+	  }
+	  @Test
+	  public void mul() {
+		  
+		  System.out.println("MUL3");
+	  }
+	  @Test
+	  public void div() {
+		  
+		  System.out.println("DIV3");
+	  }
+
+
+}
